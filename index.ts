440a1ab44374daa7483fd50e49466959adeb00c5
Jared Linares <makaveli171093@gmail.com>
2026-08-13 20:02:59 -0400

    feat: métodos funcionales y resumen de tareas

--- index.ts
+++ index.ts
@@ -19,31 +19,80 @@ let contId:number = 1;
 let sw=0;
 
 const añadirTarea = (tit: string) => {
-        tareas.push({id:contId++,titulo:tit,completada: false});
+  tareas.push({id:contId++,titulo:tit,completada: false});
 }
 
 const listarTareas = () => {
-    for (let tarea of tareas) {
-        const estado = tarea.completada ? "completada" : "pendiente";
-        console.log(`[${tarea.id}] ${tarea.titulo} - ${estado}`);
-    }
+  for (let tarea of tareas) {
+    const estado = tarea.completada ? "completada" : "pendiente";
+    console.log(`[${tarea.id}] ${tarea.titulo} - ${estado}`);
+  }
 };
+
+const listarTareasMetodo = (): void => {
+  const tareasFormateadas = tareas.map((tarea) => {
+    const { id, titulo, completada } = tarea;
+
+    return `[ID: ${id}] - ${titulo} - ${completada ? "completada" : "pendiente"}`;
+  });
+
+  tareasFormateadas.forEach((tarea) => {
+    console.log(tarea);
+  });
+};
+
+const imprimir=(lista:Tarea[]) =>{
+  const tareasFormateadas = lista.map((tarea) => {
+    const { id, titulo, completada } = tarea;
+
+    return `[ID: ${id}] - ${titulo} - ${completada ? "completada" : "pendiente"}`;
+  });
+
+  tareasFormateadas.forEach((tarea) => {
+    console.log(tarea);
+  });
+}
+
 const eliminarTarea = (id: number) => {
-    const i = tareas.findIndex(tarea => tarea.id === id);
-    if (i !== -1) {
-        const eliminar = tareas.splice(i, 1)[0];
-        console.log(`Tarea eliminada: ${eliminar.titulo}`);
-    } else {
-        console.log("Tarea no encontrada");
+  const i = tareas.findIndex(tarea => tarea.id === id);
+  if (i !== -1) {
+    const eliminar = tareas.splice(i, 1)[0];
+    console.log(`Tarea eliminada: ${eliminar.titulo}`);
+  } else {
+      console.log("Tarea no encontrada");
     }
 };
 
-while (sw !=4) {
+const marcarTarea = (id: number): void => {
+  const laTarea = tareas.find((tarea) => tarea.id === id);
+
+  if (laTarea) {
+    laTarea.completada = true;
+    console.log(`Tarea "${laTarea.titulo}" completada`);
+  } else {
+      console.log("Tarea no encontrada");
+    }
+};
+
+const filtrarPendiente = (): Tarea[] => {
+  return tareas.filter((tarea) => tarea.completada === false);
+};
+
+const filtrarCompletada = (): Tarea[] => {
+  return tareas.filter((tarea) => tarea.completada);
+};
+
+
+
+while (sw !=7) {
   const answer = await rl.question (`Elija la tarea a realizar:
     1. Agregar tarea
     2. Eliminar tarea
     3. Listar tareas
-    4. Salir
+    4. Marcar tarea completada
+    5. Filtrar tareas pendientes
+    6. Filtrar tareas completadas
+    7. Salir
     tu respuesta:  `);
     sw = parseInt(answer);
     
@@ -53,13 +102,26 @@ while (sw !=4) {
       break
     case 2:
       eliminarTarea(parseInt(await rl.question("ID de la tarea a eliminar: ")));
-      break;
+    break;
     case 3:
-      listarTareas();
+      listarTareasMetodo();
+    break;
     case 4:
-      break;
+      marcarTarea(parseInt(await rl.question("ID de la tarea completada: ")))      
+    break;
+    case 5:
+      imprimir(filtrarPendiente());
+    break;
+    case 6:
+      imprimir(filtrarCompletada());
+    break;
+    case 7:
+      console.log("ADIOS!");
+      
+    break;
   }
 }
+
 
 
 // 🚫 No eliminar las líneas de abajo ⬇️
